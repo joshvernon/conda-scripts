aba03a99eb485d7805e8d3b63c0d71f3c4d9fe27
joshvernon <jvernon_483@hotmail.com>
2016-12-28 16:21:29 -0600

    Update to include other common miniconda root prefixes

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
-import os.path
+from os.path import exists, expanduser
 
 def get_root_prefix():
     """Gets the root prefix for a user's conda installation."""
-    return os.path.expanduser('~/miniconda')
+    if exists(expanduser('~/miniconda')):
+        root_prefix = expanduser('~/miniconda')
+    elif exists(expanduser('~/miniconda2')):
+        root_prefix = expanduser('~/miniconda2')
+    else:
+        root_prefix = None
+    return root_prefix
